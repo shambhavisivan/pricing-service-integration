460f309d891d7f13062103aa4b92d6b85000f052
Marko Cekol <marko.cekol@cloudsense.com>
2020-06-04 11:05:32 +0200

    T-49788: Add APricebookSelector class
    Change-Id: I704df84abe82d099335928bf806bbb4e4f2ed66d

--- build-sfdx.sh
+++ build-sfdx.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+set -e
+echo "Starting PSI CI build on SFDX"
+
+orgAlias=PSI-CI$BUILD_NUMBER
+
+function finish() {
+	# make sure scratch org is removed before exit
+	echo "Deleting scratch org..."
+	sfdx force:org:delete --targetusername $orgAlias --noprompt
+}
+
+trap finish EXIT
+
+echo "Creating scratch org..."
+sfdx force:org:create --setdefaultusername -f config/project-scratch-def.json --setalias $orgAlias
+
+echo "Deploying source code"
+sfdx force:source:push --targetusername $orgAlias --wait 10
+
+echo "Running tests"
+sfdx force:apex:test:run --testlevel RunLocalTests --targetusername $orgAlias --resultformat tap
